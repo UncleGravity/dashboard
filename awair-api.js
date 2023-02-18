1756f96f04969edc170334f714733abdc0360b9a
Angel Viera <angel@viera.tech>
2023-02-19 00:05:16 +0800

    init commit, grafana+timescaledb+app docker setup

---
+++ awair-api.js
@@ -0,0 +1,151 @@
+const axios = require("axios");
+const fs = require("fs");
+
+// API Docs: https://docs.developer.getawair.com/?version=latest
+// Save this somewhere safer, like an .env file
+const ACCESS_TOKEN = process.env.AWAIR_ACCESS_TOKEN;
+const DEVICE_IDS = [19796, 117]; // Replace with your device IDs
+const REFRESH_INTERVAL = 60000; // 1 minute
+const DATA_INTERVAL = 300000; // 5 minutes
+const DATA_BUFFER = 10; // Number of extra data points to retrieve
+const OUTPUT_FILE = "air_data.txt";
+
+// Get latest air data for a device
+/* Returns array in this format:
+[
+  {
+    timestamp: '2023-02-14T09:09:30.000Z',
+    score: 92,
+    sensors: [ {"comp":"pm25","value":3}, {"comp":"co2","value":467}, {"comp":"temp","value":71.69}, {"comp":"humid","value":56.76} ,{"comp":"voc","value":337} ],
+    indices: [ [Object], [Object], [Object], [Object], [Object] ]
+  },
+  {
+  ...
+  },
+  ...
+]
+*/
+async function getAirData(deviceId) {
+  const config = {
+    method: "get",
+    url: `https://developer-apis.awair.is/v1/users/self/devices/awair-element/${deviceId}/air-data/raw`,
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+    params: {
+      fahrenheit: true,
+      limit: 360,
+    },
+  };
+
+  try {
+    const response = await axios(config);
+    // console.log(JSON.stringify(response.data));
+    return response.data.data.length > 0 ? extractAirData(response.data.data) : [];
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Get all devices
+/* Response looks like this:
+{
+  devices: [
+    {
+      name: 'Bedroom',
+      deviceId: 19796,
+      deviceType: 'awair-element',
+      deviceUUID: 'awair-element_19796',
+      latitude: 0,
+      longitude: 0,
+      preference: 'SLEEP',
+      locationName: '',
+      roomType: 'BEDROOM',
+      spaceType: 'HOME',
+      macAddress: '70886B14388A',
+      timezone: ''
+    },
+    {
+      name: 'Living Room',
+      deviceId: 117,
+      deviceType: 'awair-element',
+      deviceUUID: 'awair-element_117',
+      latitude: 0,
+      longitude: 0,
+      preference: 'GENERAL',
+      locationName: '',
+      roomType: 'LIVING_ROOM',
+      spaceType: 'HOME',
+      macAddress: '70886B1407B7',
+      timezone: ''
+    }
+  ]
+}
+*/
+async function getDevices() {
+  var config = {
+    method: "get",
+    url: "https://developer-apis.awair.is/v1/users/self/devices",
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await axios(config);
+    // console.log(response.data);
+    return response.data.devices.length > 0 ? response.data : [];
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Extract data from air data response
+/* Response looks like this:
+[
+  '2023-02-14T09:14:11.000Z,71.87,56.78,459,337,2',
+  '2023-02-14T09:14:01.000Z,71.798,56.78,459,335,2',
+  '2023-02-14T09:13:51.000Z,71.762,56.76,459,331,2',
+  ...
+]
+*/
+function extractAirData(response) {
+  const data = response;
+  const result = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const timestamp = data[i].timestamp;
+    const sensors = data[i].sensors;
+    const values = sensors.reduce((acc, curr) => {
+      acc[curr.comp] = curr.value;
+      return acc;
+    }, {});
+
+    const row = `${timestamp},${values.temp},${values.humid},${values.co2},${values.voc},${values.pm25}`;
+    result.push(row);
+  }
+
+  return result;
+}
+
+function saveDataToFile(airData, fileName) {
+  const fileContents = airData.join("\n").concat("\n");
+  fs.appendFileSync(fileName, fileContents);
+  console.log(`Data saved to file: ${fileName}`);
+}
+
+// run();
+// console.log(getLastTimestamp("177"))
+async function test() {
+  console.log(await getDevices())
+  console.log(await getAirData("117"))
+  // let response = await getAirData("117")
+  // console.log(response)
+  // data = extractAirData(response)
+  // console.log(data)
+  // saveDataToFile(data, "test.txt")
+}
+
+// test()
+
+module.exports = { getDevices, getAirData };
---
+++ db-test.js
@@ -0,0 +1,161 @@
+const { Client } = require("pg");
+const { getAirData} = require("./awair-api.js");
+
+// Define the connection parameters
+const connectionParams = {
+  user: "postgres",
+  host: "timescaledb",
+  database: "postgres",
+  // read password from local .env file
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432, // or the port number you specified when running the Docker container
+};
+
+async function setupTable() {
+  // Create a new PostgreSQL client instance
+  const client = new Client(connectionParams);
+
+  try {
+    // Connect to the database
+    await client.connect();
+
+    // Define the SQL queries as strings
+    const createTableQuery = `
+      CREATE TABLE awair_id_117 (   
+        timestamp TIMESTAMPTZ NOT NULL,   
+        temp FLOAT NOT NULL,   
+        humid FLOAT NOT NULL,   
+        co2 INTEGER NOT NULL,   
+        voc INTEGER NOT NULL,   
+        pm25 INTEGER NOT NULL 
+      );
+    `;
+
+    const createHypertableQuery = `
+      SELECT create_hypertable('awair_id_117', 'timestamp');
+    `;
+
+    const addUniqueConstraintQuery = `
+      ALTER TABLE awair_id_117 ADD CONSTRAINT unique_timestamp UNIQUE (timestamp);
+    `;
+
+    // Run the queries using the client instance
+    await client.query(createTableQuery);
+    console.log("Table created successfully");
+    await client.query(createHypertableQuery);
+    console.log("Hypertable created successfully");
+    await client.query(addUniqueConstraintQuery);
+    console.log("Unique constraint added successfully");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Close the client connection
+    await client.end();
+  }
+}
+
+async function save(data, tableName) {
+  // Create a new PostgreSQL client instance
+  const client = new Client(connectionParams);
+
+  try {
+    // Convert the data into an array of value strings
+    const values = data.map((entry) => {
+      const [timestamp, temp, humid, co2, voc, pm25] = entry.split(",");
+      return `('${timestamp}', ${temp}, ${humid}, ${co2}, ${voc}, ${pm25})`;
+    });
+
+    // Build the INSERT statement and execute it
+    const insertQuery = `INSERT INTO ${tableName} (timestamp, temp, humid, co2, voc, pm25)
+                           VALUES ${values.join(",")}
+                           ON CONFLICT (timestamp) DO NOTHING;`;
+
+    // Connect to the database
+    await client.connect();
+    await client.query(insertQuery);
+
+    console.log("Data inserted successfully");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Close the client connection
+    await client.end();
+  }
+}
+
+async function read(numRows, tableName) {
+  // Create a new PostgreSQL client instance
+  const client = new Client(connectionParams);
+
+  try {
+
+    // Connect to the database
+    await client.connect();
+
+    // Construct the SELECT query with the LIMIT clause
+    const selectQuery = `SELECT * FROM ${tableName} ORDER BY timestamp DESC LIMIT $1;`;
+    const values = [numRows];
+
+    // Execute the SELECT query with the specified number of rows
+    const res = await client.query(selectQuery, values);
+
+    // Log the query results to the console
+    console.log("Query results:");
+    console.log(res.rows);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Close the client connection
+    await client.end();
+  }
+}
+
+// Example array of data
+const data = [
+  "2023-02-14T12:18:48.000Z,72.69800000000001,55.44,538,903,10",
+  "2023-02-14T12:18:38.000Z,72.5,55.51,500,846,4",
+  "2023-02-14T12:18:28.000Z,72.32,56.14,493,742,3",
+  "2023-02-14T12:18:18.000Z,72.176,54.98,493,706,3",
+  "2023-02-14T12:18:08.000Z,72.158,55.26,494,687,4",
+  "2023-02-14T12:17:58.000Z,72.158,55.3,495,742,3",
+  "2023-02-14T12:17:48.000Z,72.19399999999999,55.11,494,788,3",
+];
+
+async function main() {
+  await setupTable()
+  await save(await getAirData("117"), "awair_id_117");
+  await read(500, "awair_id_117");
+  // console.log(process.env.POSTGRES_PASSWORD)
+}
+
+// run main function every 5 minutes
+setInterval(main, 300000);
+main();
+
+/*
+NOTES:
+CREATE TABLE awair_id_117 (   
+    timestamp TIMESTAMPTZ NOT NULL,   
+    temp FLOAT NOT NULL,   
+    humid FLOAT NOT NULL,   
+    co2 INTEGER NOT NULL,   
+    voc INTEGER NOT NULL,   
+    pm25 INTEGER NOT NULL 
+);
+
+SELECT create_hypertable('awair_id_117', 'timestamp');
+
+ALTER TABLE awair_id_117 ADD CONSTRAINT unique_timestamp UNIQUE (timestamp);
+
+INSERT INTO awair_id_117 (timestamp, temp, humid, co2, voc, pm25) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (timestamp) DO NOTHING;
+
+
+
+// Start timescaledb container
+// Same network as grafana
+// Using volume so database persists container deletion
+docker run -d --name timescaledb -p 5432:5432 --network grafana-timescaledb-network -e POSTGRES_PASSWORD=password -v timescaledb-storage:/var/lib/postgresql/data timescale/timescaledb:latest-pg14
+
+// 
+docker run -d --name=grafana -v grafana-storage:/var/lib/grafana --network grafana-timescaledb-network -p 3000:3000 grafana/grafana
+*/
