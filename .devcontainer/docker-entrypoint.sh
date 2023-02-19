80339174eb0635e5e2f765edfba8b618157b8736
Angel Viera <angel@viera.tech>
2023-02-19 16:11:57 +0800

    grafana datasource auto loads on container start

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+# More info about entrypoint: 
+# https://docs.docker.com/engine/reference/builder/#entrypoint
+# https://code.visualstudio.com/remote/advancedcontainers/start-processes
+
+echo "Running npm install in /workspaces/dashboard"
+cd /workspaces/dashboard
+npm run clean
+npm install
 
 echo "Waiting 10 seconds for postgres to start!"
 sleep 10
-pm2-docker start /workspaces/dashboard/db-test.js
+pm2-docker start db-test.js
 
 # run command passed in via CMD in Dockerfile
 exec "$@"
