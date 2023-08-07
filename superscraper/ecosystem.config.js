c06e03573bf6bbd1eee170628019c0a77d9f4e48
AV <angel@viera.tech>
2023-08-07 20:34:59 +0800

    enable withings on startup

--- superscraper/ecosystem.config.js
+++ superscraper/ecosystem.config.js
@@ -1,42 +1,47 @@
 module.exports = {
-    apps: [
-      {
-        name: 'awair',
-        script: 'npm',
-        args: 'run start -w superscraper-awair',
-        interpreter: 'none',
-      },
-      {
-        name: 'healthkit',
-        script: 'npm',
-        args: 'run start -w superscraper-healthkit',
-        interpreter: 'none',
-      },
-      {
-        name: 'maps',
-        script: 'npm',
-        args: 'run start -w superscraper-maps',
-        interpreter: 'none',
-      },
-      {
-        name: 'money',
-        script: 'npm',
-        args: 'run start -w superscraper-money',
-        interpreter: 'none',
-      },
-      {
-        name: 'oura',
-        script: 'npm',
-        args: 'run start -w superscraper-oura',
-        interpreter: 'none',
-      },
-      {
-        name: 'screentime',
-        script: 'npm',
-        args: 'run start -w superscraper-screentime',
-        interpreter: 'none',
-      },
-      // Add more workspaces as needed
-    ],
-  };
-  
+  apps: [
+    {
+      name: 'awair',
+      script: 'npm',
+      args: 'run start -w superscraper-awair',
+      interpreter: 'none',
+    },
+    {
+      name: 'healthkit',
+      script: 'npm',
+      args: 'run start -w superscraper-healthkit',
+      interpreter: 'none',
+    },
+    {
+      name: 'maps',
+      script: 'npm',
+      args: 'run start -w superscraper-maps',
+      interpreter: 'none',
+    },
+    {
+      name: 'money',
+      script: 'npm',
+      args: 'run start -w superscraper-money',
+      interpreter: 'none',
+    },
+    {
+      name: 'oura',
+      script: 'npm',
+      args: 'run start -w superscraper-oura',
+      interpreter: 'none',
+    },
+    {
+      name: 'withings',
+      script: 'npm',
+      args: 'run start -w superscraper-withings',
+      interpreter: 'none',
+    },
+    {
+      name: 'screentime',
+      script: 'npm',
+      args: 'run start -w superscraper-screentime',
+      interpreter: 'none',
+    },
+    // Add more workspaces as needed
+  ],
+};
